6d132b18d49151cf9cfd979b7b4d9b983762dfb3
zhangqidi <zqd13861980856@163.com>
2021-01-22 01:34:51 +0800

    student(2)

--- student/models.py
+++ student/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(null=True, max_length=20)
+    age = models.CharField(default='10', null=True, max_length=20)
